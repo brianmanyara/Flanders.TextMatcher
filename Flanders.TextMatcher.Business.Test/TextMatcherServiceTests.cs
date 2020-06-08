162b99d928884fbf609e104630f7ffe7f95b7d93
brian.manyara <brian.manyara@insight.com>
2020-06-08 23:14:35 +0800

    add unit tests

--- Flanders.TextMatcher.Business.Test/TextMatcherServiceTests.cs
+++ Flanders.TextMatcher.Business.Test/TextMatcherServiceTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using Flanders.TextMatcher.Business.Impl;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,6 +13,22 @@ namespace Flanders.TextMatcher.Business.Test
         public void Setup() 
         {
             _iTextMatcherService = new TextMatcherService();
+        }
+
+        [TestMethod]
+        public void Two_Matches_Found() 
+        {
+            var results = _iTextMatcherService.Match("table football, foosball", "foo").ToArray();
+            Assert.AreEqual(2, results.Length);
+            Assert.AreEqual(6, results[0].Position);
+            Assert.AreEqual(16, results[1].Position);
+        }
+
+        [TestMethod]
+        public void No_Matches_Found()  
+        {
+            var results = _iTextMatcherService.Match("table football, foosball", "fool").ToArray();
+            Assert.AreEqual(0, results.Length);
         }
 
     }
